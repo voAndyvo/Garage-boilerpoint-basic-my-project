c274acd54861193334e281e7ce52e869cea280f5
s4090493 <s4090493@student.rmit.edu.au>
2026-08-15 12:26:29 +1000

    fix: Fixed empty image not showing when missing photo

--- frontend/src/components/team-page/MemberImage.tsx
+++ frontend/src/components/team-page/MemberImage.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import Image from "next/image";
+import { useState } from "react";
 
 interface MemberImageProps {
     src: string
 }
 
 export function MemberImage({ src } : MemberImageProps) {
+    const [error, setError] = useState(false);
     return (
-        <Image width={200} height={200} src={src} alt={src} onError={(e) => {e.currentTarget.src="/team-page/empty.jpg"; e.currentTarget.onerror = null;}} className="rounded-full" />
+        <Image width={200} height={200} src={error ? "/empty.jpg" : src} alt={src} onError={(e) => {setError(true); e.currentTarget.onerror = null;}} className="rounded-full" />
     );
 }
